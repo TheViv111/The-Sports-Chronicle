02e16cd3e7ec428c9c673d1ba44210faaef57b1c
Vivaan Handa <vivaan.handa@pathwaysschool.in>
2025-11-21 16:29:09 +0530

    Restore XSLT stylesheet for styled sitemap view

--- scripts/build-sitemap.cjs
+++ scripts/build-sitemap.cjs
@@ -76,6 +76,7 @@ function generateSitemapXML(entries) {
   </url>`).join('\n');
 
   return `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>`;
