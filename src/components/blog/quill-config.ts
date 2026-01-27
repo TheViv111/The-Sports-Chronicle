cc88c8091105ce41774d8070e83a40683193f730
Vivaan Handa <vivaan.handa@pathwaysschool.in>
2026-01-27 12:57:54 +0530

    Colours

--- src/components/blog/quill-config.ts
+++ src/components/blog/quill-config.ts
@@ -3,8 +3,11 @@ export const modules = {
   toolbar: {
     container: [
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [
+        '#e11d48', '#2563eb', '#16a34a', '#d97706', '#7c3aed', '#db2777', '#0891b2', 
+        '#4b5563', '#ef4444', '#3b82f6', '#22c55e', '#f59e0b', '#8b5cf6', '#ec4899'
+      ] }, { 'background': [] }],
       ['bold', 'italic', 'underline', 'strike'],
-      [{ 'color': [] }, { 'background': [] }],
       [{ 'script': 'sub'}, { 'script': 'super' }],
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
       [{ 'indent': '-1'}, { 'indent': '+1' }],
